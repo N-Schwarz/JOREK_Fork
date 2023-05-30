50d05977d2899d158137a1ca696f06c45b96fdcf
Vinodh Bandaru <vkb@ipp.mpg.de>
2023-05-30 16:20:06 +0200

    Bugfix for reliable readability of Deuterium ADAS data on all platforms

--- jorek2_main.f90
+++ jorek2_main.f90
@@ -483,7 +483,7 @@ mpi_required = 0
   mhd_sim%bnd_elm_list  => bnd_elm_list
 
   ! --- Load deuterium ADAS data if required
-  if (deuterium_adas) ad_deuterium = read_adf11(my_id,'96_h') 
+  if (deuterium_adas) ad_deuterium = read_adf11(my_id,'96_h', adas_dir) 
 
    ! --- Initialize FFTW
 #ifdef USE_FFTW
