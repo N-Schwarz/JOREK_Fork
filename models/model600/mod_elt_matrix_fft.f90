8c9732151a297948bfdd6b8fa09048e06078bf8e
vkb <vkb@toki02.bc.rzg.mpg.de>
2024-05-13 12:12:30 +0200

    Bugfix related to tgnum_nre

--- models/model600/mod_elt_matrix_fft.f90
+++ models/model600/mod_elt_matrix_fft.f90
@@ -4516,7 +4516,7 @@ do i=1,n_vertex_max
                                               + (Dre_par - Dre_prof) * BigR / BB2 * Bgrad_nre_star * Bgrad_nre_nre                   * xjac * theta * tstep &
                                               + Dre_prof * BigR  * (v_x*nre_x + v_y*nre_y                      )        * xjac * theta * tstep &
                                               + Dre_perp_num  * (v_xx + v_x/BigR + v_yy) * (nre_xx + nre_x/BigR + nre_yy) * BigR     * xjac * theta * tstep &
-                                              + tgnum_nre * 0.5d0 * tstep * 0.5d0 * BigR * ( BigR**2 * (nre_x * u0_y - nre_y * u0_x) * ( v_x * u0_y - v_y * u0_x) + Vlight_adv**2 / BB2 * 1.d0/BigR**2 * (nre_x * psi_y - nre_y * psi_x ) * ( v_x * ps0_y -  v_y * ps0_x ) &
+                                              + tgnum_nre * 0.5d0 * tstep * 0.5d0 * BigR * ( BigR**2 * (nre_x * u0_y - nre_y * u0_x) * ( v_x * u0_y - v_y * u0_x) + Vlight_adv**2 / BB2 * 1.d0/BigR**2 * (nre_x * ps0_y - nre_y * ps0_x ) * ( v_x * ps0_y -  v_y * ps0_x ) &
                                                                     ) * xjac * theta * tstep
         
                         amat_n(var_nre, var_nre) =   v * Vlight_adv / F0 * ( F0 * nre_p )                                               * xjac * theta * tstep &
@@ -4525,7 +4525,7 @@ do i=1,n_vertex_max
                                                        
 
                         amat_k(var_nre, var_nre) =  (Dre_par - Dre_prof) * BigR / BB2 * Bgrad_nre_k_star * Bgrad_nre_nre         * xjac * theta * tstep &
-                                                    + tgnum_nre * 0.5d0 * tstep * 0.5d0 * BigR * ( Vlight_adv**2 / BB2 * 1.d0/BigR**2 * (nre_x * psi_y - nre_y * psi_x ) * ( F0 / BigR * v_p)    ) * xjac * theta * tstep
+                                                    + tgnum_nre * 0.5d0 * tstep * 0.5d0 * BigR * ( Vlight_adv**2 / BB2 * 1.d0/BigR**2 * (nre_x * ps0_y - nre_y * ps0_x ) * ( F0 / BigR * v_p)    ) * xjac * theta * tstep
 
                         amat_kn(var_nre, var_nre) =   (Dre_par - Dre_prof) * BigR / BB2 * Bgrad_nre_k_star * Bgrad_nre_nre_n    * xjac * theta * tstep &
                                                       + Dre_prof * BigR  * (v_p*nre_p /BigR**2 )        * xjac * theta * tstep &
